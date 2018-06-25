47ea58d1fc44b4cba07f432856676549871e3a49
qinggge <zhu_huang@126.com>
2018-06-25 08:10:01 +0800

    update

---
+++ js/sticky-topbar.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll',function(xxx){
+    if(window.scrollY > 0){
+        topNavBar.classList.add("sticky")
+    }else{
+        topNavBar.classList.remove("sticky")
+    }
+})
+
+let liTags = document.querySelectorAll('nav.menu > ul > li')
+for(let i = 0; i < liTags.length; i++){
+    liTags[i].onmouseenter = function(x){
+        x.currentTarget.classList.add('active')
+    }
+    liTags[i].onmouseleave = function(x){
+        x.currentTarget.classList.remove('active')
+    }
+}
